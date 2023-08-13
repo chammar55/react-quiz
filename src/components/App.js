e6624a993eff712825370a0998b3976ef4968400
Ch Ammar <96951639+chammar55@users.noreply.github.com>
2023-08-13 18:12:36 +0500

    13

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -17,6 +18,7 @@ const initialState = {
   index: 0, // for current question
   answer: null,
   points: 0,
+  highscore: 0,
 };
 
 function reducer(state, action) {
@@ -40,6 +42,13 @@ function reducer(state, action) {
       };
     case "nextQuestion":
       return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
     default:
       throw new Error("Action is unKnown");
   }
@@ -47,12 +56,11 @@ function reducer(state, action) {
 
 export default function App() {
   // const [state, dispatch] = useReducer(reducer, initialState);
-  const [{ questions, status, index, answer, points }, dispatch] = useReducer(
-    reducer,
-    initialState
-  ); // destructuring of state into questions and status
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState); // destructuring of state into questions and status
 
   const numQuestions = questions.length;
+
   const maxPossiblePoints = questions.reduce(
     // Total points of all questions
     (prev, cur) => prev + cur.points,
@@ -88,8 +96,20 @@ export default function App() {
               dispatch={dispatch}
               answer={answer}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
         )}
       </Main>
     </div>
--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,13 +1,25 @@
-function NextButton({ dispatch, answer }) {
+function NextButton({ dispatch, answer, index, numQuestions }) {
   if (answer === null) return null; // No answer no below code
-  return (
-    <button
-      className="btn btn-ui"
-      onClick={() => dispatch({ type: "nextQuestion" })}
-    >
-      Next
-    </button>
-  );
+  if (index < numQuestions - 1)
+    // Stop to move to next question when it reach 15
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "nextQuestion" })}
+      >
+        Next
+      </button>
+    );
+  if (index === numQuestions - 1)
+    // Stop to move to next question when it reach 15
+    return (
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "finish" })}
+      >
+        Finish
+      </button>
+    );
 }
 
 export default NextButton;
