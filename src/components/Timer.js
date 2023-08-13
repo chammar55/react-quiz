668bbf3e5609300221ae0608e6b460dd25470898
chammar55 <chammar5255@gmail.com>
2023-08-13 20:33:38 +0500

    .

---
+++ src/components/Timer.js
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+function Timer({ dispatch, secondsRemaining }) {
+  const mins = Math.floor(secondsRemaining / 60);
+  const seconds = secondsRemaining % 60;
+  useEffect(
+    function () {
+      const id = setInterval(function () {
+        dispatch({ type: "tick" });
+      }, 1000);
+      return () => clearInterval(id); // this ( cleanup function ) will stop the the timer or else it wil never stop even after we restart our quiz
+    },
+    [dispatch]
+  );
+
+  return (
+    <div className="timer">
+      {mins < 10 && "0"}
+      {mins}: {seconds < 10 && "0"}
+      {seconds}
+    </div>
+  );
+}
+
+export default Timer;
